bfbec6d77b4cd50000ce4cff2dd49c9a637dfe34
Kevin Wei <chenghaowei@gmail.com>
2020-12-07 23:43:29 -0500

    rename index page to example, remove 7-reasons page, update example page styles

--- pages/7-reasons-why/a.jsx
+++
@@ -1,5 +0,0 @@
-import PageBuilder from 'components/PageBuilder';
-
-export default function Page() {
-  return null;
-}
--- pages/example/index.jsx
+++ pages/example/index.jsx
@@ -1,6 +1,6 @@
 import PageBuilder from 'components/PageBuilder';
-import content from 'content/index.json';
-import css from 'styles/pages/index.module.scss';
+import content from 'content/example.json';
+import css from 'styles/pages/example.module.scss';
 
 export default function Page() {
   return <PageBuilder content={content} css={css}/>;
